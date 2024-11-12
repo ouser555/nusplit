e4518e80977375953800de37526baf2239909341
ouser555 <95702400+ouser555@users.noreply.github.com>
2024-11-12 18:31:04 +0800

    Add files via upload

---
+++ nusp/rev1/halconf.h
@@ -0,0 +1,7 @@
+#pragma once
+
+//#define HAL_USE_I2C TRUE
+//#define HAL_USE_SERIAL TRUE
+//#define HAL_USE_ADC TRUE
+
+#include_next <halconf.h>
---
+++ nusp/rules.mk
@@ -0,0 +1,17 @@
+DEFAULT_FOLDER = nusp/rev1
+SERIAL_DRIVER = vendor
+#DYNAMIC_KEYMAP_ENABLE = yes
+
+#OLED_ENABLE = yes
+#OLED_DRIVER = ssd1306
+#OLED_TRANSPORT = i2c
+
+#POINTING_DEVICE_ENABLE = yes
+#POINTING_DEVICE_DRIVER = custom
+#JOYSTICK_ENABLE = yes
+#JOYSTICK_DRIVER = analog
+
+#OPT_DEFS += -DHAL_USE_I2C=TRUE
+
+#SRC += drivers/sensors/analog_joystick.c
+#SRC += sub_joystick.c
